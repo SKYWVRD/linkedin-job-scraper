e8b5a87093e887f76d2684afcd125f0ad82c74bc
Sean Boonzaier <boonzaiersa@gmail.com>
2023-03-19 13:46:23 +0200

    Fix variable naming faults

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 def get_html_data(job_title: str, location: str):
     try:
-        URL = 'https://www.linkedin.com/jobs/search?keywords=Data%20Engineer&location=Ireland&geoId=104738515&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=2'
+        URL = 'https://www.linkedin.com/jobs/search?keywords=Data%20Engineer&location=Ireland&geoId=&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0'
         html_data = requests.get(URL)
 
     except: 
@@ -13,16 +13,14 @@ def get_html_data(job_title: str, location: str):
     return html_data
 
 def get_job_list_elements(html_data):
-
     try:
-        html_soup_object = BeautifulSoup(page.content, "html.parser")
-        job_list = soup.find('ul', class_='jobs-search__results-list')
-        job_list_elements = job_list_elements.find_all('li')
+        html_soup_object = BeautifulSoup(html_data.content, "html.parser")
+        job_list = html_soup_object.find('ul', class_='jobs-search__results-list')
+        job_list_elements = job_list.find_all('li')
     
     except Exception as e:
         print("Could not extract elements from HTML data")
         print('Error:', e)
-
 
     return job_list_elements
 
@@ -41,12 +39,16 @@ def extract_jobs(job_elements):
             job_location = job_location.contents[0].strip()
             job_listings.append({'id': job_id, "title": job_title,
                                 "recruiter": job_recruiter, "location": job_location})
-
         except Exception as e:
             print(e)
             continue
+        
+    return job_listings
 
-        return job_listings
+def get_jobs(position: str, location: str):
 
-
-
+    html_data = get_html_data(position, location)
+    job_listings_elements = get_job_list_elements(html_data)
+    job_dict = extract_jobs(job_listings_elements)
+    for job in job_dict:
+        print(job)
