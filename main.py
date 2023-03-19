20b03d9373a550c371e498eb1d30df32b0c1ba28
Sean Boonzaier <boonzaiersa@gmail.com>
2023-03-19 12:01:04 +0200

    Test stripping job names from h3 element

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.linkedin.com/jobs/search?keywords=&location=ireland&geoId=&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0'
+URL = 'https://www.linkedin.com/jobs/search?keywords=Data%20Engineer&location=Ireland&geoId=104738515&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0'
 
 page = requests.get(URL)
 
@@ -9,4 +9,4 @@ soup = BeautifulSoup(page.content, "html.parser")
 results = soup.find_all("h3",class_="base-search-card__title")
 
 for result in results:
-    print(result)
+    print(result.contents[0].strip())
